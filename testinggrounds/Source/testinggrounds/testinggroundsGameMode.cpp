71439394497ce68d026ecde98ca8082af136d11e
Nelson Rodrigues <nrodrigues@abyssal.eu>
2016-10-19 11:25:54 +0100

    Unreal Course: set up initial project

---
+++ testinggrounds/Source/testinggrounds/testinggroundsGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "testinggrounds.h"
+#include "testinggroundsGameMode.h"
+#include "testinggroundsHUD.h"
+#include "testinggroundsCharacter.h"
+
+AtestinggroundsGameMode::AtestinggroundsGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AtestinggroundsHUD::StaticClass();
+}
